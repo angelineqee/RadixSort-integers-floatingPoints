0a47be796507773caa8c33afd80c90a79d8928e9
Juyiillee <104578153+Juyiillee@users.noreply.github.com>
2023-05-17 15:08:56 +0800

    Update RadixSort.java
    opCounter

--- RadixSort.java
+++ RadixSort.java
@@ -10,12 +10,12 @@ import java.util.Random;
 public class RadixSort 
 {
 	//declare variables
-    	static int PlaceValue; 	//variable to store the current place value during sorting
-    	static int opCounter = 0;	//counter to track the number of primitive operations
+    static int PlaceValue; 	//variable to store the current place value during sorting
+    static int opCounter = 0;	//counter to track the number of primitive operations
     
     
 	//this method is to generate a random array elements
-    	static int[] generateRandomArray(int size, int maxValue) 
+    static int[] generateRandomArray(int size, int maxValue) 
 	{
 		int[] array = new int[size];	
 		Random random = new Random(System.currentTimeMillis());
@@ -28,86 +28,76 @@ public class RadixSort
     
 	//radix sort algorithm
 	static void sort(ArrayList<Integer>[] fromArray, ArrayList<Integer>[] toArray, int maxDigits, int counter) 
-    	{
+    {
 		opCounter++;
-		if(counter < maxDigits)	//if the current counter is less than the max number of digits
+		if(counter < maxDigits)	//if the current counter is less than the max number of digits ( 1 comparison )
 		{
-		    for(int i=0; i<10; i++) 	//iterate over each ArrayList in the fromArray
+		    for(int i=0; i<10; i++) 	//iterate over each ArrayList in the fromArray ( 1 assignment, 1 comparison, 1 arithmetic)
 		    {
-			opCounter = opCounter + 4;
-			for(int j=0; j<fromArray[i].size(); j++) 	//iterate over each element in the current ArrayList
-			{
-			    opCounter = opCounter + 4;
-			    opCounter++;
-			    if(counter == (maxDigits-1))	//if it is the last digits
-			    {
-				PlaceValue = fromArray[i].get(j)/((int)Math.pow(10, counter));	//calculate the place value
-				opCounter = opCounter + 4; //operation counter for assignment and division
-			    }
-			    else
-			    {
-				PlaceValue = fromArray[i].get(j)/((int)Math.pow(10, counter))%10;	//calculate the place value before the last digit
-				opCounter = opCounter + 5;
-			    }
-			    toArray[PlaceValue].add(fromArray[i].get(j));	//add the element to the corresponding ArrayList in the toArray
-			    opCounter = opCounter + 4;
-			}
-			fromArray[i].clear();	//clear the current ArrayList in the fromArray
-			opCounter = opCounter + 2;
+                opCounter = opCounter + 3;
+                for(int j=0; j<fromArray[i].size(); j++) 	//iterate over each element in the current ArrayList ( 1 assignment, 1 comparison, 1 arithmetic)
+                {
+                    opCounter = opCounter + 3;
+                    opCounter++;
+                    if(counter == (maxDigits-1))	//if it is the last digits (1 comparison)
+                    {
+                        PlaceValue = fromArray[i].get(j)/((int)Math.pow(10, counter));	//calculate the place value ( 1 assignment, 1 arithmetic )
+                        opCounter = opCounter + 2; //operation counter for assignment and division
+                    }
+                    else
+                    {
+                        PlaceValue = fromArray[i].get(j)/((int)Math.pow(10, counter))%10;	//calculate the place value before the last digit ( 1 assignment, 2 arithmetic )
+                        opCounter = opCounter + 3;
+                    }
+                    toArray[PlaceValue].add(fromArray[i].get(j));	//add the element to the corresponding ArrayList in the toArray
+                }
+                fromArray[i].clear();	//clear the current ArrayList in the fromArray
 		    }
-		    sort(toArray, fromArray, maxDigits, counter+1);
-		    opCounter = opCounter + 1;
+		    sort(toArray, fromArray, maxDigits, counter+1); //( 1 calling method )
+		    opCounter++ ;
 		}
 	}
     
 	//method to find the largest number of digits in the array
-    	public static int findLargest(int[] arr) 
-    	{
-        	int max = Integer.MIN_VALUE;	//assign the min number to max
-		opCounter++;
-        	int maxDigits = 0;
-		opCounter++;
-        	for (int i = 0; i < arr.length; i++) 
-        	{
-	    		opCounter = opCounter + 4;
-	    		opCounter++;
-			//assign the current array element to max if it is larger than the current max
-            		if (arr[i] > max) 
-            		{
-				max = arr[i];
-				maxDigits = (int)Math.log10(max) + 1;
-				opCounter = opCounter + 5;	    
-		    	}
-        	}
-		opCounter++;
-        	return maxDigits;
-    	}
+    public static int findLargest(int[] arr) 
+    {
+        int max = Integer.MIN_VALUE;	//assign the min number to max
+        int maxDigits = 0;
+        for (int i = 0; i < arr.length; i++) 
+        {
+            //assign the current array element to max if it is larger than the current max
+            if (arr[i] > max) 
+            {
+                max = arr[i];
+                maxDigits = (int)Math.log10(max) + 1;
+            }
+        }
+        return maxDigits;
+    }
     
 	public static void main(String args[]) throws IOException  
 	{
 		//create 2 arrays to hold the digits during sorting
 		@SuppressWarnings("unchecked")
 		ArrayList<Integer>[] array1 = new ArrayList[10];
-		opCounter++;
 		@SuppressWarnings("unchecked")
 		ArrayList<Integer>[] array2 = new ArrayList[10];
-		opCounter++;
 		
-	    	//create a new csv file to store the results
-	    	//there are no incrementation of opCounter in the file processing section as it is not counted as the algorithm of sorting
-    		File csvWriter = new File("results.csv");
-    		try 
+        //create a new csv file to store the results
+        //there are no incrementation of opCounter in the file processing section as it is not counted as the algorithm of sorting
+        File csvWriter = new File("results.csv");
+        try 
 		{
-            		if (csvWriter.exists()) 
+            if (csvWriter.exists()) 
 			{
 				csvWriter.delete(); // Delete the existing CSV file
 				csvWriter.createNewFile(); // Create a new empty CSV file
 			} 
-        	} 
+        } 
 		catch (IOException e) 
 		{
-            		System.out.println("An error occurred: " + e.getMessage());
-        	}
+    		System.out.println("An error occurred: " + e.getMessage());
+        }
     	
 		FileWriter fileWriter = new FileWriter(csvWriter, true);
 		fileWriter.append("Number of inputs");
@@ -115,7 +105,7 @@ public class RadixSort
 		fileWriter.append("Number of operations");
 		fileWriter.append("\n");
 
-		for (int size = 5; size<=200; size+=5) //input 
+		for (int size = 2; size<=300; size = size + 5)
 		{
 			int[] input = generateRandomArray(size, 1000);	//generate random array elements
 
@@ -129,39 +119,31 @@ public class RadixSort
 	        
 			for (int i = 0; i < 10; i++) 
 			{
-			    opCounter = opCounter + 4;
 			    array1[i] = new ArrayList<Integer>();
 			    array2[i] = new ArrayList<Integer>();
-			    opCounter = opCounter + 2;
 			}
 	        
 			//use array1 as buckets, put the elements from input array into array1 based on their smallest place value
 			for(int i=0; i<input.length; i++) 
 			{
-			    opCounter = opCounter + 4;
 			    PlaceValue = input[i] % 10;
 			    array1[PlaceValue].add(input[i]);
-			    opCounter = opCounter + 6;
+                opCounter = opCounter + 5;
 			}
 	        
 		
 			int maxDigits = findLargest(input);
-			opCounter = opCounter + 2;
 			//sort the array elements
 			sort(array1, array2, maxDigits, 1);
 			opCounter ++;
-
-			opCounter = opCounter + 2;
 			
-			//if the number of digit in the number is even number then it will get from array 2
+			//if the number of digit in the number is even number then it will get from array 2 ( 1 comparison )
 			if(maxDigits % 2 == 0)
 			{
 			    for(int i=0; i<10; i++) 
 			    { 
-					opCounter = opCounter + 4;
 					for(int j=0; j<array2[i].size(); j++) 
 					{
-						opCounter = opCounter + 4;
 						System.out.print(array2[i].get(j)+" ");
 					}
 			    }
@@ -171,10 +153,8 @@ public class RadixSort
 			{
 				for(int i=0; i<10; i++) 
 				{
-					opCounter = opCounter + 4;
 					for(int j=0; j<array1[i].size(); j++) 
 					{
-						opCounter = opCounter + 4;
 						System.out.print(array1[i].get(j)+" ");
 					}
 				}
@@ -182,17 +162,21 @@ public class RadixSort
 			}
 			System.out.println();
 			//display the number of operations of the code
+            System.out.println("No of input: " +size);
 			System.out.println("Operation: " +opCounter);
+            System.out.println();
+
 
 			//write the number of inputs and number of operations of each iteration into the csv file to generate graph later
-		    	fileWriter.write(String.valueOf(size));
+		    fileWriter.write(String.valueOf(size));
 		   	fileWriter.write(",");
-		    	fileWriter.write(String.valueOf(opCounter));
-		    	fileWriter.write("\n");
+		    fileWriter.write(String.valueOf(opCounter));
+		    fileWriter.write("\n");
 		    
+            opCounter = 0;
 		}
-    		fileWriter.flush();
-    		fileWriter.close();
+    	fileWriter.flush();
+    	fileWriter.close();
     	
 	}
         
